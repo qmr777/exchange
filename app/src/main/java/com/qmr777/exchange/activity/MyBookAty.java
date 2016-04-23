d2d011074830f7da2c0dbeda6c0e07cf0a7a7c8c
qmr <qmr777@163.com>
2016-04-24 02:07:09 +0800

    20160424

---
+++ app/src/main/java/com/qmr777/exchange/activity/MyBookAty.java
@@ -0,0 +1,12 @@
+package com.qmr777.exchange.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyBookAty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/qmr777/exchange/model/MyBook.java
@@ -0,0 +1,7 @@
+package com.qmr777.exchange.model;
+
+/**
+ * Created by qmr on 2016/4/24.
+ */
+public class MyBook {
+}
